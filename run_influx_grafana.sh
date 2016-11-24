1860285a87d94e49bd33b41b45b37c91f016557e
rharnoor <rharnoor@cisco.com>
2016-11-24 19:56:54 +0530

    clubbing apt-get installs

--- run_influx_grafana.sh
+++ run_influx_grafana.sh
@@ -4,5 +4,5 @@ GRAFANA_PORT=${GRAFANA_PORT:=3000}
 INFLUX_PORT=${INFLUX_PORT:=8086}
 docker run -d -p $GRAFANA_PORT:3000 -p $INFLUX_PORT:8086 graflux
 
-echo "test grafana on your machine by running curl http://DOCKER_HOST_IP/$GRAFANA_PORT "
+echo "test grafana on your machine by running curl http://DOCKER_HOST_IP:$GRAFANA_PORT "
 echo "test influx db on your machine by running curl -sl -I DOCKER_HOST_IP:$INFLUX_PORT/ping"
