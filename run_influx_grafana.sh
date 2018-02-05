dc58316c335849c9812457bdb98ae538f89ae66f
Raghavendra Harnoorkar <raghavendra.harnoorkar@gmail.com>
2018-02-05 21:54:42 +0530

    Update run_influx_grafana.sh

--- run_influx_grafana.sh
+++ run_influx_grafana.sh
@@ -2,6 +2,7 @@
 
 GRAFANA_PORT=${GRAFANA_PORT:=3000}
 INFLUX_PORT=${INFLUX_PORT:=8086}
+
 docker run -d -p $GRAFANA_PORT:3000 -p $INFLUX_PORT:8086 graflux
 
 echo "test grafana on your machine by running curl http://DOCKER_HOST_IP:$GRAFANA_PORT "
